b1e81d65e62f2dd67515b1dabf3a77be2683f6bc
David <dwander616@gmail.com>
2025-03-23 11:51:05 -0300

    fixing carousel transition and styling status bar

--- App.tsx
+++ App.tsx
@@ -4,21 +4,22 @@ import {
   Roboto_700Bold,
   Roboto_400Regular
 } from '@expo-google-fonts/roboto';
-import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { GluestackUIProvider, useTheme } from '@gluestack-ui/themed';
 
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
 
+const statusBarColor = config.tokens.colors?.violet500 || "#8b5cf6";
+
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular})
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular});
 
   return (
     <GluestackUIProvider config={config}>
       <StatusBar 
         barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent
+        backgroundColor={statusBarColor}  
       />
 
       {fontsLoaded ? <SignIn /> : <Loading />}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,53 @@
-import { VStack, Image } from '@gluestack-ui/themed';
+import { useState, useEffect, useRef } from 'react';
+import { VStack } from '@gluestack-ui/themed';
+import { Animated } from 'react-native';
 
-import BackgroundImg from '@assets/photo01.png';
+// Imagens do carrossel
+import Image1 from '@assets/photo01.png';
+import Image2 from '@assets/photo02.png';
+import Image3 from '@assets/photo03.png';
+import Image4 from '@assets/photo04.png';
+import Image5 from '@assets/photo05.png';
+import Image6 from '@assets/photo06.png';
+import Image7 from '@assets/photo07.png';
+import Image8 from '@assets/photo08.png';
+import Image9 from '@assets/photo09.png';
+
+const images = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9];
 
 export function SignIn() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 800, 
+        useNativeDriver: true,
+      }).start(() => {
+       
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 800, 
+          useNativeDriver: true,
+        }).start();
+      });
+    }, 8000); 
+
+    return () => clearInterval(interval);
+  }, [fadeAnim]);
+
   return (
     <VStack flex={1} bg="$gray400">
-      <Image 
-        w="$full"
-        h={624}
-        source={BackgroundImg} 
-        defaultSource={BackgroundImg}
+      <Animated.Image
+        style={{ width: '100%', height: 550, opacity: fadeAnim }}
+        source={images[currentIndex]}
         alt="Carrossel de imagens"
       />
     </VStack>
-  )
+  );
 }
